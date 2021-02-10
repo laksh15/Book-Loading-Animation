db0d7b7fb4520ff528840c41483a5f272ea3b830
Lakshay Bhatia <lakshaybhatia999@gmail.com>
2021-02-10 19:34:39 +1100

    Initial Commit

---
+++ Book Loading Animation/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Book Loading Animation
+//
+//  Created by Lakshay Bhatia on 10/2/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
